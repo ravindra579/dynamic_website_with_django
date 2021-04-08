8e2b6ee3b977ab0b6a3bb755531410872b336378
ravin <ravindrareddy1217@gmail.com>
2021-04-09 01:51:22 +0530

    first

--- dj/dj/settings.py
+++ dj/dj/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '2c0*8t^jq^+wneiqqa22l2@59pjqmaf57717#!lvg%b=d@hqf8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'setforth.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'set-forth.herokuapp.com']
 
 
 # Application definition
